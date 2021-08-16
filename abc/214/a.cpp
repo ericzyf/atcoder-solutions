3532f7888329b8604f4872cd6f1aca73fc53bcd7
Yifan Zhang <yfzhang1999@gmail.com>
2021-08-16 21:37:36 +0800

    abc214.a

---
+++ abc/214/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 125) {
+        cout << 4;
+    } else if (n <= 211) {
+        cout << 6;
+    } else {
+        cout << 8;
+    }
+    cout << endl;
+}
